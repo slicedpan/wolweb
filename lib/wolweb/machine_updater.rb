93ada09fdd18d39d126a31d293b20e27ed273639
Owen Mooney <omooney@tcd.ie>
2022-03-18 17:29:03 +0000

    updated ping invocation to use timeout (the -W flag seems to be unreliable in some cases)

--- lib/wolweb/machine_updater.rb
+++ lib/wolweb/machine_updater.rb
@@ -1,7 +1,7 @@
 module WolWeb
   class MachineUpdater
     def self.call(machine)
-      machine.service_statuses[:network] = system("ping -c 1 -W 0.25 #{machine.ip_address}")
+      machine.service_statuses[:network] = system("timeout 0.25 ping -c 1 -W 0.25 #{machine.ip_address}")
       machine.service_statuses[:steam] = system("timeout 0.25 nc -z #{machine.ip_address} 27036")
     end
   end
