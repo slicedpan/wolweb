ca4fd0714e69219b662320cdf71b8865600067c1
Owen Mooney <omooney@tcd.ie>
2022-03-18 17:07:51 +0000

    tidied up the interface a bit

--- public/index.js
+++ public/index.js
@@ -11,4 +11,28 @@ $(document).ready(function() {
       })
     })
   });
+
+  function updateMachine(id, serviceStatuses) {
+    var machineElement = $(`.machine-container[machine-id=${id}]`);
+    var wakeupButtonDisabled = false;
+    for (const [service, up] of Object.entries(serviceStatuses)) {
+      if (up) {
+        wakeupButtonDisabled = true;
+        machineElement.find(`.service-${service}`).addClass("service-up").removeClass("service-down");
+      } else {
+        machineElement.find(`.service-${service}`).addClass("service-down").removeClass("service-up")
+      }
+    }
+    machineElement.find(".wakeup-button").prop("disabled", wakeupButtonDisabled);
+  }
+
+  window.setInterval(function() {
+    $.ajax("/machines").done(function(response) { 
+      response.machines.forEach(machine => {
+        updateMachine(machine.id, machine.service_statuses)
+      });  
+    }).fail(function() {
+      console.error("Request to /machines failed!");
+    })
+  }, 10000)
 });
